a26735e24f6af30abc1cef13176bb841dfe6ea73
DhanushSS3 <dhanush777ss@gmail.com>
2026-03-24 14:44:09 +0530

    Removed the duplicate file

--- auth-service/admin-service/src/lib/logger.ts
+++
@@ -1,6 +0,0 @@
-import pino from 'pino';
-export const logger = pino(
-  process.env.NODE_ENV === 'development'
-    ? { transport: { target: 'pino-pretty', options: { colorize: true } } }
-    : {},
-);
--- auth-service/auth-service/src/lib/logger.ts
+++
@@ -1,9 +0,0 @@
-import pino from 'pino';
-import { config } from '../config/env';
-
-export const logger = pino({
-  level: config.nodeEnv === 'production' ? 'info' : 'debug',
-  ...(config.nodeEnv !== 'production' && {
-    transport: { target: 'pino-pretty', options: { colorize: true } },
-  }),
-});
